227747d5c1dbc50c974a700a27ec229af29834b6
sliel-dev <yaewom@gmail.com>
2025-09-02 12:20:49 +0900

    num add

---
+++ num/num1.py
@@ -0,0 +1,16 @@
+# 숫자데이터 만들기
+# 정수
+print(0)
+print(100)
+
+# 실수
+print(1.23)
+print(100.11)
+
+# 부호
+print(+10)
+print(-10)
+
+# 타입 확인
+print(type(100))
+print(type(1.23))
---
+++ num/quiz1.py
@@ -0,0 +1,7 @@
+print(25 + 7)
+print(100 - 45)
+print(12 * 8)
+print(4 ** 2)
+print(17 / 5)
+print(17 // 5) # 몫
+print(17 % 5) # 나머지
