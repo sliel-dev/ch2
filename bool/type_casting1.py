09d4b7d667397a2be0e1f9e95085ccdee536d2fe
sliel-dev <yaewom@gmail.com>
2025-09-02 12:53:41 +0900

    bool add

---
+++ bool/type_casting1.py
@@ -0,0 +1,21 @@
+# 실수를 정수로 변환
+print(int(3.23))
+
+# 문자를 정수로 변환
+print(int('323'))
+
+# 정수를 실수로 변환
+print(float(323))
+
+# 문자를 실수로 변환
+print(float('3.23'))
+
+# 정수를 참,거짓으로 변환
+print(bool(0))
+print(bool(1)) # 1 이상의 값은 True로 반환
+
+# 정수를 문자로 변환
+print(str(123))
+
+# 실수를 문자로 변환
+print(str(3.14))
