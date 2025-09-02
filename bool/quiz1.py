f300b63e5a29f0d24bba84df6a2d57e41d59b623
sliel-dev <yaewom@gmail.com>
2025-09-02 14:45:21 +0900

    bool add

---
+++ bool/quiz1.py
@@ -0,0 +1,3 @@
+print(int('75') + 25)
+print(int(float('36.5') + 1.5))
+print(str(2025) + '년')
---
+++ bool/quiz2.py
@@ -0,0 +1,7 @@
+d = 1.2
+f = 0.9
+
+i = int(d)              # 1
+i2 = int(f)             # 0
+i3 = int(d) + int(f)    # 1
+i4 = int(d + f)         # 2
